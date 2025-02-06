813e486bf42380e8ca18298cf2237aee7c455a86
andrewpjlewis <lewiand1234@gmail.com>
2025-02-06 16:09:14 -0700

    W05 prove

--- src/js/parkService.mjs
+++ src/js/parkService.mjs
@@ -220,6 +220,7 @@ async function getJson(url) {
 }
 
 export function getInfoLinks(data) {
+  // Why index + 2 below? no real reason. we don't want index 0 since that is the one we used for the banner...I decided to skip an image.
   const withUpdatedImages = parkInfoLinks.map((item, index) => {
     item.image = data[index + 2].url;
     return item;
@@ -227,6 +228,17 @@ export function getInfoLinks(data) {
   return withUpdatedImages;
 }
 
-export function getParkData() {
-  return park;
+export async function getParkData() {
+  const parkData = await getJson("parks?parkCode=yell ");
+  return parkData.data[0];
+}
+
+export async function getParkAlerts(code) {
+  const parkData = await getJson(`alerts?parkCode=${code}`);
+  return parkData.data;
+}
+
+export async function getParkVisitorCenters(code) {
+  const parkData = await getJson(`visitorcenters?parkCode=${code}`);
+  return parkData.data;
 }
